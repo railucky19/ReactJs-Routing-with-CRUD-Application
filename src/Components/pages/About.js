a926c39c966b7383cf4ee644ca0aa9e728764afe
Lucky Rai <luckrai9@gmail.com>
2020-11-01 23:13:45 +0530

    First Commit

---
+++ src/Components/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          lorem40In reprehenderit consectetur aute incididunt nisi. Ex
+          reprehenderit dolore irure ipsum velit qui excepteur occaecat. Nisi
+          esse ad cillum voluptate fugiat enim aliqua fugiat laborum consectetur
+          eu aute ut qui. Officia velit do duis incididunt incididunt irure et
+          irure qui.
+        </p>
+        <p className="lead">
+          lorem40In reprehenderit consectetur aute incididunt nisi. Ex
+          reprehenderit dolore irure ipsum velit qui excepteur occaecat. Nisi
+          esse ad cillum voluptate fugiat enim aliqua fugiat laborum consectetur
+          eu aute ut qui. Officia velit do duis incididunt incididunt irure et
+          irure qui.
+        </p>
+        <p className="lead">
+          lorem40In reprehenderit consectetur aute incididunt nisi. Ex
+          reprehenderit dolore irure ipsum velit qui excepteur occaecat. Nisi
+          esse ad cillum voluptate fugiat enim aliqua fugiat laborum consectetur
+          eu aute ut qui. Officia velit do duis incididunt incididunt irure et
+          irure qui.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default About;
